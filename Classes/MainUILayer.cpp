61c182f708d44475547f667e56d8403b0d69eb2a
CatNBox <damian.bonggi@gmail.com>
2016-08-14 19:08:53 +0900

    Done MainScene load sprites without animation
    Done MainScene load sprites without animation

--- Classes/MainUILayer.cpp
+++ Classes/MainUILayer.cpp
@@ -57,14 +57,14 @@ void MainUILayer::initPcSelectUI()
 	selectPcLogo->setName("pcLogo");
 	this->addChild(selectPcLogo, 2);
 
-	//캐릭터 로드
+	//캐릭터 스프라이트 로드
 	//소드맨
 	auto swordmanNormal = Sprite::create("img/swordmanStand.png");
 	auto swordmanSelected = Sprite::createWithSpriteFrame(swordmanNormal->getSpriteFrame());
 	auto swordmanDisabled = Sprite::createWithSpriteFrame(swordmanNormal->getSpriteFrame());
 	auto swordmanSelectedFrame = Sprite::create("img/selectedFrame.png");
 	swordmanSelected->setAnchorPoint(Vec2(0, 0));
-	swordmanSelected->setScale(0.6);
+	swordmanSelected->setScale(0.5);
 	swordmanSelectedFrame->setScale(2.0);
 	swordmanSelectedFrame->addChild(swordmanSelected,0);
 	//아쳐
@@ -73,7 +73,7 @@ void MainUILayer::initPcSelectUI()
 	auto archerDisabled = Sprite::createWithSpriteFrame(swordmanNormal->getSpriteFrame());
 	auto archerSelectedFrame = Sprite::createWithSpriteFrame(swordmanSelectedFrame->getSpriteFrame());
 	archerSelected->setAnchorPoint(Vec2(0, 0));
-	archerSelected->setScale(0.6);
+	archerSelected->setScale(0.5);
 	archerSelectedFrame->setScale(2.0);
 	archerSelectedFrame->addChild(archerSelected, 0);
 	//위자드
@@ -82,7 +82,7 @@ void MainUILayer::initPcSelectUI()
 	auto wizardDisabled = Sprite::createWithSpriteFrame(swordmanNormal->getSpriteFrame());
 	auto wizardSelectedFrame = Sprite::createWithSpriteFrame(swordmanSelectedFrame->getSpriteFrame());
 	wizardSelected->setAnchorPoint(Vec2(0, 0));
-	wizardSelected->setScale(0.6);
+	wizardSelected->setScale(0.5);
 	wizardSelectedFrame->setScale(2.0);
 	wizardSelectedFrame->addChild(wizardSelected, 0);
 
@@ -98,19 +98,19 @@ void MainUILayer::initPcSelectUI()
 	pcJob02->setPosition(Vec2(320, 180));
 	pcJob03->setPosition(Vec2(520-50, 180));
 
-	auto pMenu01 = Menu::create(pcJob01, pcJob02, pcJob03, NULL);
+	auto pSelectPcObj = Menu::create(pcJob01, pcJob02, pcJob03, NULL);
 
-	pMenu01->setPosition(Vec2(0, 0));
-	pMenu01->setName("pcSelectObj");
+	pSelectPcObj->setPosition(Vec2(0, 0));
+	pSelectPcObj->setName("selectPcObj");
 
-	this->addChild(pMenu01,3);
+	this->addChild(pSelectPcObj,2);
 }
 
 void MainUILayer::initBossSelectUI()
 {
 	//selectPcLogo 찾아서 제거
 	this->removeChildByName("pcLogo");
-	this->removeChildByName("pcSelectObj");
+	this->removeChildByName("selectPcObj");
 
 	//셀렉트로고
 	auto selecBossLogo = Sprite::create("img/selectMonster_Logo.png");
@@ -119,16 +119,55 @@ void MainUILayer::initBossSelectUI()
 	selecBossLogo->setName("bossLogo");
 	this->addChild(selecBossLogo, 2);
 
-	auto item1 = MenuItemFont::create("BoooooooooooooooooooSS", CC_CALLBACK_1(MainUILayer::doStartGameSelect, this));
-	
-	item1->setColor(Color3B(255, 255, 255));
-	item1->setPosition(Vec2(300, 100));
+	//보스 스프라이트 로드
+	//베히
+	auto behimothNormal = Sprite::create("img/behimoth.png");
+	auto behimothSelected = Sprite::createWithSpriteFrame(behimothNormal->getSpriteFrame());
+	auto behimothDisabled = Sprite::createWithSpriteFrame(behimothNormal->getSpriteFrame());
+	auto behimothSelectedFrame = Sprite::create("img/selectMonsterFrame.png");
+	behimothSelected->setAnchorPoint(Vec2(0, 0));
+	behimothSelected->setScale(0.5);
+	behimothSelectedFrame->setScale(2.0);
+	behimothSelectedFrame->addChild(behimothSelected, 0);
+	//레비
+	auto LeviathanNormal = Sprite::create("img/Leviathan.png");
+	auto LeviathanSelected = Sprite::createWithSpriteFrame(LeviathanNormal->getSpriteFrame());
+	auto LeviathanDisabled = Sprite::createWithSpriteFrame(LeviathanNormal->getSpriteFrame());
+	auto LeviathanSelectedFrame = Sprite::createWithSpriteFrame(behimothSelectedFrame->getSpriteFrame());
+	LeviathanSelected->setAnchorPoint(Vec2(0, 0));
+	LeviathanSelected->setScale(0.5);
+	LeviathanSelectedFrame->setScale(2.0);
+	LeviathanSelectedFrame->addChild(LeviathanSelected, 0);
+	//라드
+	auto radragonNormal = Sprite::create("img/radragon.png");
+	auto radragonSelected = Sprite::createWithSpriteFrame(radragonNormal->getSpriteFrame());
+	auto radragonDisabled = Sprite::createWithSpriteFrame(radragonNormal->getSpriteFrame());
+	auto radragonSelectedFrame = Sprite::createWithSpriteFrame(behimothSelectedFrame->getSpriteFrame());
+	radragonSelected->setAnchorPoint(Vec2(0, 0));
+	radragonSelected->setScale(0.5);
+	radragonSelectedFrame->setScale(2.0);
+	radragonSelectedFrame->addChild(radragonSelected, 0);
 
-	auto pMenu = Menu::create(item1, NULL);
-	pMenu->setPosition(Vec2(0, 0));
+	auto boss01 = MenuItemSprite::create(behimothNormal, behimothSelectedFrame, behimothDisabled, CC_CALLBACK_1(MainUILayer::doStartGameSelect, this));
+	auto boss02 = MenuItemSprite::create(LeviathanNormal, LeviathanSelectedFrame, LeviathanDisabled, CC_CALLBACK_1(MainUILayer::doStartGameSelect, this));
+	auto boss03 = MenuItemSprite::create(radragonNormal, radragonSelectedFrame, radragonDisabled, CC_CALLBACK_1(MainUILayer::doStartGameSelect, this));
 
-	this->addChild(pMenu, 2);
+	boss01->setPosition(Vec2(320, 180));
+	boss02->setPosition(Vec2(470, 180));
+	boss03->setPosition(Vec2(150, 180));
+
+	boss01->setScale(0.5);
+	boss02->setScale(0.5);
+	boss03->setScale(0.5);
+
+	auto pSelectBossObj = Menu::create(boss01, boss02, boss03, NULL);
+	pSelectBossObj->setPosition(Vec2(0, 0));
+
+	this->addChild(pSelectBossObj, 2);
 }
+//****************************
+/* 연출 함수 따로 만들 것 */
+//****************************
 
 void MainUILayer::doStartGameSelect(Ref* pSender)
 {
