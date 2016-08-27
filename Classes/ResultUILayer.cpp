d45f04e9e65b2b5f95dd4074b193b89fddd217e5
CatNBox <damian.bonggi@gmail.com>
2016-08-28 01:54:51 +0900

    Done visualize ResultScene
    Done visualize ResultScene

--- Classes/ResultUILayer.cpp
+++ Classes/ResultUILayer.cpp
@@ -22,26 +22,35 @@ void ResultUILayer::initResultUI()
 
 	auto pMenu = Menu::create(item1, NULL);
 
-	pMenu->setPosition(Point(400, 30));
+	pMenu->setPosition(Vec2(400, 30));
 	pMenu->setAnchorPoint(Vec2(0, 0));
 
 	this->addChild(pMenu,2);
 	//임시메뉴코드 끝
 
-	auto sprSceneLogo = Sprite::create("img/Result_Logo.png");
+	auto sprSceneLogo = Sprite::create("img/result_Logo.png");
+	sprSceneLogo->setAnchorPoint(Vec2(0, 0));
+	sprSceneLogo->setPosition(Vec2(60, 295));
+
 	auto sprBoard = Sprite::create("img/CharacterSelectBoard.png");
+	sprBoard->setAnchorPoint(Vec2(0.5, 0.5));
+	sprBoard->setPosition(Vec2(320, 180));
 
 	this->addChild(sprBoard, 1);
 	this->addChild(sprSceneLogo, 2);
 	
 	if (winGame)
 	{
-		auto sprBtlResultLogo = Sprite::create("img/Succeed_Logo.png");
+		auto sprBtlResultLogo = Sprite::create("img/succed_logo.png");
+		sprBtlResultLogo->setAnchorPoint(Vec2(0.5, 0.5));
+		sprBtlResultLogo->setPosition(Vec2(320, 180));
 		this->addChild(sprBtlResultLogo, 2);
 	} 
 	else
 	{
-		auto sprBtlResultLogo = Sprite::create("img/FAILED_LOGO.png");
+		auto sprBtlResultLogo = Sprite::create("img/FAILED_logo.png");
+		sprBtlResultLogo->setAnchorPoint(Vec2(0.5, 0.5));
+		sprBtlResultLogo->setPosition(Vec2(320, 180));
 		this->addChild(sprBtlResultLogo, 2);
 	}
 }
